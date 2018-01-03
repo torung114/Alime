5dadaffbc02209a7789bcba438ab3306a8b97844
Trung Nguyen <torung114@gmail.com>
2018-01-03 21:36:20 +0700

    minor

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,3 @@
-server 'your_server_ip', port: your_port_num, roles: [:web, :app, :db], primary: true
-
 set :repo_url,        'git@github.com:torung114/Alime.git'
 set :application,     'alime'
 set :puma_threads,    [4, 16]
@@ -8,7 +6,6 @@ set :puma_workers,    0
 # Don't change these unless you know what you're doing
 set :pty,             true
 set :use_sudo,        false
-set :stage,           :production
 set :deploy_via,      :remote_cache
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
@@ -16,7 +13,6 @@ set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
